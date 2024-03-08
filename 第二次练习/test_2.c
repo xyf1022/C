7fd52ed01c0c779f3f4dd179a43095999dc50923
xyf1022 <162397556+xyf1022@users.noreply.github.com>
2024-03-08 16:41:35 +0800

    Add files via upload

---
+++ 第二次练习/test_2.c
@@ -0,0 +1,74 @@
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//打印小飞机
+//int main() {
+//
+//	printf("     **     \n");
+//	printf("     **     \n");
+//	printf("************\n");
+//	printf("************\n");
+//	printf("    *  *    \n");
+//	printf("    *  *    \n");
+//
+//	return 0;
+	//
+
+//}
+
+
+//int main() {
+//	//打印学生信息
+//	printf("Name    Age    Gender\n");
+//	printf( "---------------------\n" );
+//	printf("Jack     18     Man\n");
+//
+//	return 0;
+//
+//
+//
+//}
+
+//int main() {
+//	int a = 40;
+//
+//	int c = 212;
+//
+//	int answer = (-8 + 22) * a - 10 + c / 2;
+//
+//	printf("answer is %d\n",answer);
+//
+//	return 0;
+//
+//
+//}
+int Max(int x, int y)
+{
+	if (x > y)
+		return x;
+	else
+		return y;
+
+}
+
+
+
+
+
+int main() {
+	int a = 0;
+
+	int b = 0;
+
+	scanf("%d%d",&a,&b);
+
+	int r = Max(a,b);
+
+	printf("the max nummber is %d\n",r);
+	
+	
+
+
+
+	return 0;
+}
